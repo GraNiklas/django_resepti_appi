afe50f80a93dfebf866e6019bc2e4534a96a5d3c
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-08-13 16:22:21 +0300

    fix for superuser exists fail

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py createsuperuser --no-input
+# python manage.py createsuperuser --no-input
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); \
+if not User.objects.filter(username='admin').exists(): \
+    User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')"
